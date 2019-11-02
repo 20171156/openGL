ffa4f10ca55b7a7f9874325f97764a1952db9799
성은 박 <tjd8555@naver.com>
2019-11-03 02:39:33 +0900

    -파일입출력 완료

--- openGL/main.cpp
+++ openGL/main.cpp
@@ -22,20 +22,6 @@ GLFWwindow* window;
 using namespace glm;
 using namespace std;
 
-struct POINT3D {
-	float x;
-	float y;
-	float z;
-};
-POINT3D *vertex;
-
-struct FACE4D {
-	int v1;
-	int v2;
-	int v3;
-};
-FACE4D *face;
-
 GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path) {
 
 	// 쉐이더들 생성
@@ -126,16 +112,7 @@ GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_pat
 	return ProgramID;
 }
 
-void FILE_Load(void)
-{
-	FILE *fp;
-	int NumVertex;
 
-	fp = fopen("cube.txt", "r");
-	fscanf(fp, "%d\n", &NumVertex);
-
-
-}
 
 int main(void)
 {
@@ -209,125 +186,160 @@ int main(void)
 	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 	glm::mat4 MVP1 = Projection * View * Model1;
 
+
+	FILE* Sun_fp;
+	FILE* Moon_fp;
+	FILE* Color_fp;
+
+	GLfloat Sun[36 * 3] = { 0 };
+	GLfloat Moon[36 * 3] = { 0 };
+	GLfloat Color[36 * 3] = { 0 };
+
+	fopen_s(&Sun_fp, "sun.txt", "r");
+	fopen_s(&Moon_fp, "moon.txt", "r");
+	fopen_s(&Color_fp, "color.txt", "r");
+
+
+	if (Sun_fp == NULL)
+	{
+		fprintf(stdout, "파일을 열지 못했습니다.");
+		exit(1);
+	}
+
+	for (int i = 0; i < 36 * 3; ++i)
+	{
+		fscanf_s(Sun_fp, "%f", &Sun[i]);
+	}
+
+	for (int i = 0; i < 36 * 3; ++i)
+	{
+		fscanf_s(Moon_fp, "%f", &Moon[i]);
+	}
+
+	for (int i = 0; i < 36 * 3; ++i)
+	{
+		fscanf_s(Color_fp, "%f", &Color[i]);
+	}
+
 	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat Sun[] = {
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f
-	};
+	//static const GLfloat Sun[] = {
+	//	-1.0f,-1.0f,-1.0f,
+	//	-1.0f,-1.0f, 1.0f,
+	//	-1.0f, 1.0f, 1.0f,
+	//	 1.0f, 1.0f,-1.0f,
+	//	-1.0f,-1.0f,-1.0f,
+	//	-1.0f, 1.0f,-1.0f,
+	//	 1.0f,-1.0f, 1.0f,
+	//	-1.0f,-1.0f,-1.0f,
+	//	 1.0f,-1.0f,-1.0f,
+	//	 1.0f, 1.0f,-1.0f,
+	//	 1.0f,-1.0f,-1.0f,
+	//	-1.0f,-1.0f,-1.0f,
+	//	-1.0f,-1.0f,-1.0f,
+	//	-1.0f, 1.0f, 1.0f,
+	//	-1.0f, 1.0f,-1.0f,
+	//	 1.0f,-1.0f, 1.0f,
+	//	-1.0f,-1.0f, 1.0f,
+	//	-1.0f,-1.0f,-1.0f,
+	//	-1.0f, 1.0f, 1.0f,
+	//	-1.0f,-1.0f, 1.0f,
+	//	 1.0f,-1.0f, 1.0f,
+	//	 1.0f, 1.0f, 1.0f,
+	//	 1.0f,-1.0f,-1.0f,
+	//	 1.0f, 1.0f,-1.0f,
+	//	 1.0f,-1.0f,-1.0f,
+	//	 1.0f, 1.0f, 1.0f,
+	//	 1.0f,-1.0f, 1.0f,
+	//	 1.0f, 1.0f, 1.0f,
+	//	 1.0f, 1.0f,-1.0f,
+	//	-1.0f, 1.0f,-1.0f,
+	//	 1.0f, 1.0f, 1.0f,
+	//	-1.0f, 1.0f,-1.0f,
+	//	-1.0f, 1.0f, 1.0f,
+	//	 1.0f, 1.0f, 1.0f,
+	//	-1.0f, 1.0f, 1.0f,
+	//	 1.0f,-1.0f, 1.0f
+	//};
 
-	static const GLfloat Moon[] = {
-		-1.0f+6.0f ,-1.0f,-1.0f,
-		-1.0f+6.0f ,-1.0f, 1.0f,
-		-1.0f+6.0f , 1.0f, 1.0f,
-		 1.0f+6.0f , 1.0f,-1.0f,
-		-1.0f+6.0f ,-1.0f,-1.0f,
-		-1.0f+6.0f , 1.0f,-1.0f,
-		 1.0f+6.0f ,-1.0f, 1.0f,
-		-1.0f+6.0f ,-1.0f,-1.0f,
-		 1.0f+6.0f ,-1.0f,-1.0f,
-		 1.0f+6.0f , 1.0f,-1.0f,
-		 1.0f+6.0f ,-1.0f,-1.0f,
-		-1.0f+6.0f ,-1.0f,-1.0f,
-		-1.0f+6.0f ,-1.0f,-1.0f,
-		-1.0f+6.0f , 1.0f, 1.0f,
-		-1.0f+6.0f , 1.0f,-1.0f,
-		 1.0f+6.0f ,-1.0f, 1.0f,
-		-1.0f+6.0f ,-1.0f, 1.0f,
-		-1.0f+6.0f ,-1.0f,-1.0f,
-		-1.0f+6.0f , 1.0f, 1.0f,
-		-1.0f+6.0f ,-1.0f, 1.0f,
-		 1.0f+6.0f ,-1.0f, 1.0f,
-		 1.0f+6.0f , 1.0f, 1.0f,
-		 1.0f+6.0f ,-1.0f,-1.0f,
-		 1.0f+6.0f , 1.0f,-1.0f,
-		 1.0f+6.0f ,-1.0f,-1.0f,
-		 1.0f+6.0f , 1.0f, 1.0f,
-		 1.0f+6.0f ,-1.0f, 1.0f,
-		 1.0f+6.0f , 1.0f, 1.0f,
-		 1.0f+6.0f , 1.0f,-1.0f,
-		-1.0f+6.0f , 1.0f,-1.0f,
-		 1.0f+6.0f , 1.0f, 1.0f,
-		-1.0f+6.0f , 1.0f,-1.0f,
-		-1.0f+6.0f , 1.0f, 1.0f,
-		 1.0f+6.0f , 1.0f, 1.0f,
-		-1.0f+6.0f , 1.0f, 1.0f,
-		 1.0f+6.0f ,-1.0f, 1.0f
-	};
+	//static const GLfloat Moon[] = {
+	//	5.0f ,-1.0f,-1.0f,
+	//	5.0f ,-1.0f, 1.0f,
+	//	5.0f , 1.0f, 1.0f,
+	//	7.0f , 1.0f,-1.0f,
+	//	5.0f ,-1.0f,-1.0f,
+	//	5.0f , 1.0f,-1.0f,
+	//	7.0f ,-1.0f, 1.0f,
+	//	5.0f ,-1.0f,-1.0f,
+	//	7.0f ,-1.0f,-1.0f,
+	//	7.0f , 1.0f,-1.0f,
+	//	7.0f ,-1.0f,-1.0f,
+	//	5.0f ,-1.0f,-1.0f,
+	//	5.0f ,-1.0f,-1.0f,
+	//	5.0f , 1.0f, 1.0f,
+	//	5.0f , 1.0f,-1.0f,
+	//	7.0f ,-1.0f, 1.0f,
+	//	5.0f ,-1.0f, 1.0f,
+	//	5.0f ,-1.0f,-1.0f,
+	//	5.0f , 1.0f, 1.0f,
+	//	5.0f ,-1.0f, 1.0f,
+	//	7.0f ,-1.0f, 1.0f,
+	//	7.0f , 1.0f, 1.0f,
+	//	7.0f ,-1.0f,-1.0f,
+	//	7.0f , 1.0f,-1.0f,
+	//	7.0f ,-1.0f,-1.0f,
+	//	7.0f , 1.0f, 1.0f,
+	//	7.0f ,-1.0f, 1.0f,
+	//	7.0f , 1.0f, 1.0f,
+	//	7.0f , 1.0f,-1.0f,
+	//	5.0f , 1.0f,-1.0f,
+	//	7.0f , 1.0f, 1.0f,
+	//	5.0f , 1.0f,-1.0f,
+	//	5.0f , 1.0f, 1.0f,
+	//	7.0f , 1.0f, 1.0f,
+	//	5.0f , 1.0f, 1.0f,
+	//	7.0f ,-1.0f, 1.0f
+	//};
 
 	// One color for each vertex. They were generated randomly.
-	static const GLfloat g_color_buffer_data[] = {
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-		0.597f,  0.770f,  0.761f,
-		0.559f,  0.436f,  0.730f,
-		0.359f,  0.583f,  0.152f,
-		0.483f,  0.596f,  0.789f,
-		0.559f,  0.861f,  0.639f,
-		0.195f,  0.548f,  0.859f,
-		0.014f,  0.184f,  0.576f,
-		0.771f,  0.328f,  0.970f,
-		0.406f,  0.615f,  0.116f,
-		0.676f,  0.977f,  0.133f,
-		0.971f,  0.572f,  0.833f,
-		0.140f,  0.616f,  0.489f,
-		0.997f,  0.513f,  0.064f,
-		0.945f,  0.719f,  0.592f,
-		0.543f,  0.021f,  0.978f,
-		0.279f,  0.317f,  0.505f,
-		0.167f,  0.620f,  0.077f,
-		0.347f,  0.857f,  0.137f,
-		0.055f,  0.953f,  0.042f,
-		0.714f,  0.505f,  0.345f,
-		0.783f,  0.290f,  0.734f,
-		0.722f,  0.645f,  0.174f,
-		0.302f,  0.455f,  0.848f,
-		0.225f,  0.587f,  0.040f,
-		0.517f,  0.713f,  0.338f,
-		0.053f,  0.959f,  0.120f,
-		0.393f,  0.621f,  0.362f,
-		0.673f,  0.211f,  0.457f,
-		0.820f,  0.883f,  0.371f,
-		0.982f,  0.099f,  0.879f
-	};
+	//static const GLfloat Color[] = {
+	//	0.583f,  0.771f,  0.014f,
+	//	0.609f,  0.115f,  0.436f,
+	//	0.327f,  0.483f,  0.844f,
+	//	0.822f,  0.569f,  0.201f,
+	//	0.435f,  0.602f,  0.223f,
+	//	0.310f,  0.747f,  0.185f,
+	//	0.597f,  0.770f,  0.761f,
+	//	0.559f,  0.436f,  0.730f,
+	//	0.359f,  0.583f,  0.152f,
+	//	0.483f,  0.596f,  0.789f,
+	//	0.559f,  0.861f,  0.639f,
+	//	0.195f,  0.548f,  0.859f,
+	//	0.014f,  0.184f,  0.576f,
+	//	0.771f,  0.328f,  0.970f,
+	//	0.406f,  0.615f,  0.116f,
+	//	0.676f,  0.977f,  0.133f,
+	//	0.971f,  0.572f,  0.833f,
+	//	0.140f,  0.616f,  0.489f,
+	//	0.997f,  0.513f,  0.064f,
+	//	0.945f,  0.719f,  0.592f,
+	//	0.543f,  0.021f,  0.978f,
+	//	0.279f,  0.317f,  0.505f,
+	//	0.167f,  0.620f,  0.077f,
+	//	0.347f,  0.857f,  0.137f,
+	//	0.055f,  0.953f,  0.042f,
+	//	0.714f,  0.505f,  0.345f,
+	//	0.783f,  0.290f,  0.734f,
+	//	0.722f,  0.645f,  0.174f,
+	//	0.302f,  0.455f,  0.848f,
+	//	0.225f,  0.587f,  0.040f,
+	//	0.517f,  0.713f,  0.338f,
+	//	0.053f,  0.959f,  0.120f,
+	//	0.393f,  0.621f,  0.362f,
+	//	0.673f,  0.211f,  0.457f,
+	//	0.820f,  0.883f,  0.371f,
+	//	0.982f,  0.099f,  0.879f
+	//};
 
 	GLuint Sun_vertexbuffer;
 	glGenBuffers(1, &Sun_vertexbuffer);
@@ -337,7 +349,7 @@ int main(void)
 	GLuint colorbuffer;
 	glGenBuffers(1, &colorbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Color), Color, GL_STATIC_DRAW);
 
 	GLuint Moon_vertexbuffer;
 	glGenBuffers(1, &Moon_vertexbuffer);
